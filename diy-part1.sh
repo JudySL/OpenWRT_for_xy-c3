34d6ea8135935d64ee23bb704737ba2ee424272c
JudySL <58675509+JudySL@users.noreply.github.com>
2023-02-11 15:45:55 +0800

    Update diy-part1.sh
    Add git.openwrt.org feed

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,12 +12,16 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '/helloworld/d' feeds.conf.default
+# sed -i '/helloworld/d' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git darkmatter git://github.com/apollo-ng/luci-theme-darkmatter.git' >>feeds.conf.default
+echo 'src-git packages https://git.openwrt.org/feed/packages.git' >>feeds.conf.default
+echo 'src-git luci https://git.openwrt.org/project/luci.git' >>feeds.conf.default
+echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
