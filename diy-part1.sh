e0ab5721dc35cc46ae21bda82ccf1623597959f8
JudySL <58675509+JudySL@users.noreply.github.com>
2023-02-11 21:38:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,7 +21,3 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git darkmatter git://github.com/apollo-ng/luci-theme-darkmatter.git' >>feeds.conf.default
-echo 'src-git packages https://git.openwrt.org/feed/packages.git' >>feeds.conf.default
-echo 'src-git luci https://git.openwrt.org/project/luci.git' >>feeds.conf.default
-echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
